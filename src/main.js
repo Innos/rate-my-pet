bdf6d0906d6942b13ffd611fc7a7adcd09daff92
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2025-03-20 20:41:17 +0200

    login template and routing setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import page from "page";
+
+import layoutView from "./views/layout";
 import homeView from "./views/home";
 import petsView from "./views/pets";
-import layoutView from "./views/layout";
+import loginView from "./views/login";
 
 // Middlewares
 page(layoutView);
@@ -9,6 +11,7 @@ page(layoutView);
 // Setup routes
 page("/", homeView);
 page("/pets", petsView);
+page("/login", loginView);
 
 // Start routing
 page();
